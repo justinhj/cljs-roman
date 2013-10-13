b6f905faeb2147643fb8ca0aeb924af495387f37
Justin Heyes-Jones <justinhj@gmail.com>
2013-10-12 20:33:41 -0700

    Some tidy up Added some tests but they're not working

--- test/roman/core_test.clj
+++ test/roman/core_test.clj
@@ -1,7 +1,19 @@
 (ns roman.core-test
   (:use clojure.test
-        roman.core))
+        roman.main))
 
-(deftest a-test
-  (testing "FIXME, I fail."
-    (is (= 0 1))))
+(deftest roman-1-test
+  (testing "basic roman to integer test"
+    (is (= (roman-numerals-to-decimal "XI") 11))))
+
+(deftest roman-2-test
+  (testing "basic roman to integer test 2"
+    (is (= (roman-numerals-to-decimal "X") 11))))
+
+(deftest test-fail-me
+  (testing "basic roman to integer test 3"
+    (is (= 1 0))))
+
+
+
+
